1252a662d048c88c00a104eb5f10258b012e5982
mmunarriz <mmunarriz@gmail.com>
2022-09-08 17:20:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nextjs Blog
+
+## Build
+
+`npm run build` creates an optimized production build.
+
+## Development
+
+`npm run dev` starts the application in development mode with hot-code reloading, error reporting, and more:
+
+The application will start at http://localhost:3000 by default. The default port can be changed with -p, like so:
+
+`npx next dev -p 4000`
+
+## Production
+
+`npm run start` starts the application in production mode. The application should be compiled with next build first.
+
+The application will start at http://localhost:3000 by default. The default port can be changed with -p, like so:
+
+`npx next start -p 4000`
---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I'm a JAMstack Developer and Technical Writer who currently works in
+          Developer Relations at <a href="https://hashnode.com">Hashnode</a>. I'm passionate about communities,
+          documentation, web VR experiences, developing JAMstack + PWA
+          applications with a significant focus on accessibility alongside
+          supporting and sustaining FOSS.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog Posts</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href="/posts/[id]" as={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>          
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
